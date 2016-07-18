19c23b795708efb9727a976cac2b0d5f74a83dbf
feelobot <felix.a.rod@gmail.com>
2016-07-18 18:04:27 +0200

    fix config loading

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sed -i 's@SYSLOG_ES_URL@'$SYSLOG_ES_URL'@' /etc/syslog-ng/es.conf
+sed -i 's@SYSLOG_ES_URL@'$SYSLOG_ES_URL'@' /etc/syslog-ng/syslog-ng.conf
+sed -i 's@SYSLOG_ES_PORT@'$SYSLOG_ES_PORT'@' /etc/syslog-ng/syslog-ng.conf
 /usr/sbin/syslog-ng -F "$@"
